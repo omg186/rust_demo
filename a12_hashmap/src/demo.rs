47eab9e20ef86eb9cf37bac9f4c67723ebc68463
omg186 <haizhou_dong@163.com>
2022-11-16 21:21:37 +0800

    a12_hashmap

---
+++ a12_hashmap/src/demo.rs
@@ -0,0 +1,40 @@
+// Topic:HashMap
+//
+// Requirements:
+// * Print the name and number of items in stock for a furniture store
+// * If the number of items is 0 print "out of stock" instead of 0
+// * The store has:
+//      * 5 Chairs
+//      * 3 Beds
+//      * 2 Tables
+//      * 0 Couches
+// * Print the total number of items in stock
+//
+// Notes:
+// * Use a HashMap for the furniture store stock
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+
+    #[test]
+    fn it_works() {
+        let mut stock = HashMap::new();
+        stock.insert("Chairs", 5);
+        stock.insert("Beds", 3);
+        stock.insert("Tables", 35);
+        stock.insert("Couches", 0);
+        let mut total_stock = 0;
+        for (item, qty) in stock.iter() {
+            total_stock += qty;
+            let stock_count = if qty == &0 {
+                "out of stock".to_owned()
+            } else {
+                format!("{:?}", qty)
+            };
+            println!("item={:?},stock={:?}", item, stock_count);
+        }
+        println!("total stock:{:?}", total_stock);
+        // println!("{:?}", stock);
+    }
+}
---
+++ a12_hashmap/src/main.rs
@@ -0,0 +1,32 @@
+use std::collections::HashMap;
+mod demo;
+#[derive(Debug)]
+struct Contents {
+    content: String,
+}
+
+fn main() {
+    println!("Hello, world!");
+    let mut lockers = HashMap::new();
+    lockers.insert(
+        1,
+        Contents {
+            content: "stuff".to_owned(),
+        },
+    );
+    lockers.insert(
+        2,
+        Contents {
+            content: "shirt".to_owned(),
+        },
+    );
+    lockers.insert(
+        3,
+        Contents {
+            content: "gym shorts".to_owned(),
+        },
+    );
+    for (locker_number, content) in lockers.iter() {
+        println!("number:{:?},content:{:?}", locker_number, content.content);
+    }
+}
